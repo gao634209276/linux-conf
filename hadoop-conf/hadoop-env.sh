e6c557a0e041a3327a3724c1670611b84c3af403
hadoop <634209276@qq.com>
2016-08-28 21:41:50 +0800

    cp from hadoop1 ha

--- hadoop-conf/hadoop-env.sh
+++ hadoop-conf/hadoop-env.sh
@@ -47,8 +47,8 @@ done
 #export HADOOP_NAMENODE_INIT_HEAPSIZE=""
 
 # Extra Java runtime options.  Empty by default.
-# export HADOOP_OPTS="$HADOOP_OPTS -Djava.net.preferIPv4Stack=true"
-export HADOOP_OPTS="-Djava.library.path=$HADOOP_PREFIX/lib:$HADOOP_PREFIX/lib/native"
+export HADOOP_OPTS="$HADOOP_OPTS -Djava.net.preferIPv4Stack=true"
+
 # Command specific options appended to HADOOP_OPTS when specified
 export HADOOP_NAMENODE_OPTS="-Dhadoop.security.logger=${HADOOP_SECURITY_LOGGER:-INFO,RFAS} -Dhdfs.audit.logger=${HDFS_AUDIT_LOGGER:-INFO,NullAppender} $HADOOP_NAMENODE_OPTS"
 export HADOOP_DATANODE_OPTS="-Dhadoop.security.logger=ERROR,RFAS $HADOOP_DATANODE_OPTS"
